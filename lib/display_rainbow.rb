545c07f1a199be724304be96fc69f6e200e29167
captivating-script-3337 <rchow3illinois@gmail.com>
2021-01-05 04:11:48 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
 def display_rainbow(colors)
-  puts ""['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']""
+  puts "['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']"
 end
